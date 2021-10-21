9e7a887fb9584a41afc983c48204d9f8afcb6878
MonzonFrancisco <82822508+MonzonFrancisco@users.noreply.github.com>
2021-10-21 09:58:55 -0500

    Add files via upload

---
+++ config/actualizarcita.php
@@ -0,0 +1,31 @@
+<?php
+
+    include("conexion.php");
+        
+        $id = $_POST['id'];
+        $name = $_POST['name'];
+        $number = $_POST['number'];
+        $symptoms = $_POST['symptoms'];
+        $date = $_POST['date'];
+        $time = $_POST['time'];
+    
+        //actualizando los datos 
+        $actualizar = "UPDATE citas SET name = '$name', number = '$number', symptoms = '$symptoms', 
+        date = '$date', time = '$time' WHERE  id='$id'";
+    
+            $resultado= mysqli_query($conexion, $actualizar);
+    
+                if ($resultado) {
+                    echo "<script>
+                    alert('Datos actualizados con exito');
+                    window.location= '../public/home.php'
+                    </script>";
+                } else {
+                    echo "<script>
+                    alert('Tenemos problemas, ingrese los datos correctos');
+                    window.location= '../public/home.php'
+                    </script>";   
+                }
+
+    
+ ?>
